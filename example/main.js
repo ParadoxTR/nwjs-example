4f75c0b5aff9b7331021dc0a53a450d473675bcd
ParadoxTR <57668682+ParadoxTR@users.noreply.github.com>
2021-03-19 22:12:15 +0300

    Update main.js

--- example/main.js
+++ example/main.js
@@ -1,1 +1,12 @@
 // YOU CAN CREATE FUNCTIONS HERE
+//
+//
+// FOR EXAMPLE
+//
+// var fs = require("fs")
+// function savefile() {
+// fs.open('test.txt', 'w', function (err, file) {
+//   if (err) throw err;
+//   console.log('Saved!');
+// });
+// }
